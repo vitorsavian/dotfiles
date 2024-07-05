d4f51c9dd77bb56036c906c34e543a8f985f7f9f
Vitor Savian <vitor.savian@suse.com>
2024-07-05 02:04:41 -0300

    Added all my dotfiles

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
